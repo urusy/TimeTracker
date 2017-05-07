530f7ea94ba1d8e27e0c66d044efce79e16311dd
urusy <urusy.dev@gmail.com>
2017-05-07 15:56:59 +0900

    ListViewから過去の記録を呼び出して再開と更新処理を追加

--- TimeTracker/MainWindow.xaml.cs
+++ TimeTracker/MainWindow.xaml.cs
@@ -20,8 +20,9 @@ namespace TimeTracker
         {
             InitializeComponent();
 
-            list = new ObservableCollection<Statictics>();
+            this.list = new ObservableCollection<Statictics>();
             this.StaticticsListView.DataContext = list;
+            //this.list = this.StaticticsListView.DataContext;
 
             this.StartButton.IsEnabled = true;
             this.FinishButton.IsEnabled = false;
@@ -31,11 +32,14 @@ namespace TimeTracker
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
             var button = (Button)sender;
+            // タイマーを動かす
             if (button.Content.Equals("Start"))
             {
+                // 新規に開始する場合
                 if (this.statictics == null)
                 {
                     this.statictics = new Statictics();
+                    this.statictics.Id = Guid.NewGuid().ToString();
                     this.statictics.Start = DateTime.Now;
 
                     this.timer = new DispatcherTimer(DispatcherPriority.Normal);
@@ -43,6 +47,7 @@ namespace TimeTracker
                     this.timer.Tick += new EventHandler(this.DispatcherTimer_Tick);
                     this.timer.Start();
                 }
+                // 一時停止を解除する場合
                 else
                 {
                     this.timer.Start();
@@ -53,6 +58,7 @@ namespace TimeTracker
 
                 this.FinishButton.IsEnabled = true;
             }
+            // 一時停止する場合
             else
             {
                 this.timer.Stop();
@@ -68,7 +74,6 @@ namespace TimeTracker
         private void FinishButton_Click(object sender, RoutedEventArgs e)
         {
             this.statictics.Finish = DateTime.Now;
-            //this.statictics.Time = (this.statictics.Finish.Ticks - this.statictics.Start.Ticks) / TimeSpan.TicksPerSecond;
             this.statictics.Time = this.secondCounter;
             this.statictics.Title = this.TitleTextBox.Text.Equals(String.Empty) ? "No title" : this.TitleTextBox.Text;
 
@@ -79,27 +84,65 @@ namespace TimeTracker
             this.StartButton.IsEnabled = true;
             this.FinishButton.IsEnabled = false;
 
-            this.list.Add(this.statictics);
+            int i = 0;
+            bool hitFlg = false;
+            foreach (Statictics item in this.list)
+            {
+                if (item.Id.Equals(this.statictics.Id))
+                {
+                    this.list[i] = this.statictics;
+                    hitFlg = true;
+                    this.StaticticsListView.Items.Refresh();
+                    break;
+                }
+                i++;
+            }
+
+            // 該当する行がない場合、追加
+            if (!hitFlg) this.list.Add(this.statictics);
 
             this.secondCounter = 0;
             TimeSpan ts = new TimeSpan(0, 0, (int)this.secondCounter);
             this.TimeLabel.Content = ts.ToString();
             this.StartButton.Content = "Start";
             this.statictics = null;
+
+            this.TitleTextBox.Text = String.Empty;
         }
 
         private void DispatcherTimer_Tick(object sender, EventArgs e)
         {
             this.secondCounter++;
-            //TimeSpan ts = new TimeSpan(0, 0, (int)((DateTime.Now.Ticks - this.statictics.Start.Ticks) / TimeSpan.TicksPerSecond));
             TimeSpan ts = new TimeSpan(0, 0, (int)this.secondCounter);
 
             this.TimeLabel.Content = ts.ToString();
+        }
+        
+        private void StaticticsListView_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ListViewItem item = sender as ListViewItem;
+            object obj = item.Content;
+
+            this.statictics = (Statictics)obj;
+
+            this.TitleTextBox.Text = statictics.Title;
+            this.secondCounter = statictics.Time;
+            TimeSpan ts = new TimeSpan(0, 0, (int)this.secondCounter);
+            this.TimeLabel.Content = ts.ToString();
+            this.StartButton.Content = "Start";
+            this.TitleTextBox.IsEnabled = true;
+            this.FinishButton.IsEnabled = true;
+
+            // タイマーを設定
+            this.timer = new DispatcherTimer(DispatcherPriority.Normal);
+            this.timer.Interval = new TimeSpan(0, 0, 1);
+            this.timer.Tick += new EventHandler(this.DispatcherTimer_Tick);
         }
     }
 
     public class Statictics
     {
+        public string Id { get; set; }
         public string Title { get; set; }
         public DateTime Start { get; set; }
         public DateTime Finish { get; set; }
